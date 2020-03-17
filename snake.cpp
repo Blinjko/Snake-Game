140d8eca34d8311f1c6231c63dcbbcc4dcd52f13
Blinjko <blinjko@gmail.com>
2020-03-17 15:44:16 -0400

    Added moveSegment function to snake.cpp Added print member function to snake class Added move member function to snake class

--- snake.cpp
+++ snake.cpp
@@ -27,6 +27,30 @@ struct Segment
 	}
 };
 
+// moves a segment based upon its movements
+void moveSegment(Segment &s)
+{
+	switch(s.nextMove)
+	{
+		case Direction::UP:
+		--s.y;
+		break;
+
+		case Direction::DOWN:
+		++s.y;
+		break;
+
+		case Direction::LEFT:
+		--s.x;
+		break;
+
+		case Direction::RIGHT:
+		++s.x;
+		break;
+	}
+}
+
+
 // snake constructor
 Snake::Snake(char headSymbol, char bodySymbol, int size)
 	: m_size{size}
@@ -40,6 +64,7 @@ Snake::Snake(char headSymbol, char bodySymbol, int size)
 	}
 }
 
+// snake deconstructor
 Snake::~Snake()
 {
 	for(Segment *element: segments)
@@ -47,3 +72,45 @@ Snake::~Snake()
 		delete element;
 	}
 }
+
+// prints the snake
+void Snake::print(WINDOW *win) const
+{
+	for(Segment *element: segments)
+	{
+		mvwprintw(win, element->y, element->x, "%c", element->symbol);
+	}
+}
+
+
+// moves the snake
+void Snake::move()
+{
+	for(int i{0}; i<m_size; ++i)
+	{
+		if(i == 0)
+		{
+			moveSegment(*segments.at(i)); // move the head
+			segments.at(i)->lastMove = segments.at(i)->nextMove;
+		}
+		else
+		{
+			moveSegment(*segments.at(i)); // move a body segment
+			segments.at(i)->lastMove = segments.at(i)->nextMove; // set the last lastMove to the move just made
+			segments.at(i)->nextMove = segments.at(i-1)->lastMove; // set the nextMove to the move just made by the segment before
+		}
+	}
+}
+
+
+bool isDead() const;
+
+void addSegment(Segment s); // segment is provided
+void addSegment(); // nothing is provided and the segment will be made accordingly
+void removeSegment(); // pop a segment off the back
+
+Segment*& at(int index); // get a particular segment at an index
+
+std::vector<Segment*>& get(); // returns the Segment vector
+
+int size() const { return m_size; }
